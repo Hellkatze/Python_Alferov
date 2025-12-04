02ef8f008b65577a15e63e89d8aaf40907fb0d58
bio <senya@cinegiraffe.ru>
2025-12-04 14:19:43 +0300

    g

--- ls_8.py
+++ ls_8.py
@@ -1,8 +1,9 @@
-input_dict = {'A, E, I, O, U, L, N, S, T, R, А, В, Е, И, Н, О, Р, С, Т':1, 'D, G, Д, К, Л, М, П, У':2, 'B, C, M, P, Б, Г, Ё, Ь, Я':3, 'F, H, V, W, Y, Й, Ы':4, 'K, Ж, З, Х, Ц, Ч':5, 'J, X, Ш, Э, Ю':8, 'Q, Z, Ф, Щ, Ъ':10}
-s = 0
-a = set(input())
-for i in a:
-    for j, value in input_dict.items():
-        if i in j:
-            s+=value
-print (s)
+from random import randint
+massiv = [randint(0,1000) for i in range(10)]
+# g =0
+for i in range(len(massiv)-1):
+   for j in range (len(massiv)-1-i):
+    if massiv[j] > massiv[j+1]:
+        massiv[j], massiv[j+1] = massiv[j+1], massiv[j]
+        #print (massiv)
+print (massiv)
